20ade033e1987ae2b5aac1295529b8050d86dac9
Neilansh Chauhan <neilanshchauhan4@gmail.com>
2023-08-26 21:59:41 +0530

    Search Insert Position

---
+++ Arrays/Search_Insert.py
@@ -0,0 +1,19 @@
+# Binary Search => (O(logn))
+
+class Solution:
+    def searchInsert(self, nums, target):
+        l,r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l+r)//2
+
+            if target == nums[mid]:
+                return mid
+
+            if target > nums[mid]:
+                l = mid + 1
+
+            else:
+                r = mid - 1
+
+        return l
